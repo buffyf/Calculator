6ce79f567756d9e4ab8f714fbe8a3a5aa46fda41
Buffy Feinstein <buffyfeinstein@gmail.com>
2017-08-03 10:37:47 -0500

    html and javascript finished for basic calc

---
+++ main.js
@@ -0,0 +1,11 @@
+function btnplus()
+{document.calculator.display.value += "+";}
+function btnsub()
+{document.calculator.display.value += "-";}
+function btnmult()
+{document.calculator.display.value += "*";}
+function btndiv()
+{document.calculator.display.value += "/";}
+function btnclear() 
+    {document.calculator.display.value = "";}
+
